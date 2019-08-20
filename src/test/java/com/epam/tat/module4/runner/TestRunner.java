6c3ac9ab65e999decb6ff796d6de3a7c202e01fe
Yekaterina Karakulina <yekaterina_karakulina@epam.com>
2019-08-20 11:24:50 +0600

    pom.xml TestRunner

--- src/test/java/com/epam/tat/module4/runner/TestRunner.java
+++ src/test/java/com/epam/tat/module4/runner/TestRunner.java
@@ -1,6 +1,5 @@
 package com.epam.tat.module4.runner;
 
-import org.testng.TestListenerAdapter;
 import org.testng.TestNG;
 import org.testng.xml.XmlSuite;
 
@@ -10,7 +9,6 @@ import java.util.List;
 
 public class TestRunner {
     public static void main(String[] args) {
-        TestListenerAdapter tla = new TestListenerAdapter();
         TestNG tng = new TestNG();
         XmlSuite suite = new XmlSuite();
         suite.setName("TmpSuite");
